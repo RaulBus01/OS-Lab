d515a2b8511a0092a51b1585bf2ac7daf03584ba
RaulBus <raul.bus@student.upt.ro>
2023-03-30 11:24:39 +0300

    LinkFile options
    Added functionality for all regular file commands
    Added functionality for all links commands
    Fixed unreachable code

--- Program.c
+++ Program.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <ctype.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -9,53 +9,68 @@
 #include <stdint.h>
 #include <time.h>
 #include <sys/sysmacros.h>
-
+#include <fcntl.h>
+char choice[32];
+void regFileMenu(struct stat sb, char *argv[])
+{
+        
+    printf("Regular File %s \n", argv[1]);
+    printf("Menu for regular file \n");
+    printf("1. Read -n \n");
+    printf("2. Size -d \n");
+    printf("3. Number of hard links -h \n");
+    printf("4. Time of last modification -m \n");
+    printf("5. Access rights -a \n");
+    printf("6. Create a symbolic link give:link name -l \n");
+                  
+           
+}
+void symFileMenu(struct stat sb, char *argv[])
+{
+        printf("Symbolic Link %s \n", argv[1]);
+        printf("Menu for symbolic link \n");
+        printf("1. Link name -n \n");
+        printf("2. Delete link -l \n");
+        printf("3. Size of the link -d \n");
+        printf("4. Size of the target -t \n");
+        printf("5. Access rights -a \n");
+          
+}
 int main(int argc, char *argv[])
 {
     struct stat sb;
-    char choice[7];
+    
            if (argc != 2) {
                fprintf(stderr, "Usage: %s <pathname>\n", argv[0]);
                exit(EXIT_FAILURE);
            }
-            
-
+        
            if (lstat(argv[1], &sb) == -1) {
                perror("lstat");
                exit(EXIT_FAILURE);
            }
            if (S_ISREG(sb.st_mode))
            {
-               printf("regular file %s \n", argv[1]);
-               printf("Menu for regular file \n");
-               printf("1. Read -n \n");
-               printf("2. Size -d \n");
-               printf("3. Number of hard links -h \n");
-               printf("4. Time of last modification -m \n");
-               printf("5. Access rights -a \n");
-               printf("6. Create a symbolic link give:link name -l \n");
-               
+            regFileMenu(sb,&argv[1]);
            }
            if(S_ISLNK(sb.st_mode))
-           {
-               printf("symbolic link %s \n", argv[1]);
-               printf("Menu for symbolic link \n");
-               printf("1. Link name -n \n");
-               printf("2. Delete link -l \n");
-               printf("3. Size of the link -d \n");
-               printf("4. Size of the target -t \n");
-               printf("5. Access rights -a \n");
-               
-           }
-           scanf("%s", choice);
+            {
+            symFileMenu(sb,&argv[1]);
+            }
+            scanf("%s",choice); 
+            system("clear");
+           
+            printf("Your choice %s  \n",choice);
+           
            int i = 0;
-           while(choice[i] != '\0')
+           while(choice[i] != '\0') 
            {
+            
                 if(choice[0] == '-')
                 {
-                    if ( i!=0 && strchr("nldhma", choice[i]))
+                    if ( i!=0  && S_ISREG(sb.st_mode))
                     {
-                        switch(choice[i])
+                        switch(tolower(choice[i]))
                         {
                             case 'n':
                                 printf("File name : %s \n",argv[1]);
@@ -64,28 +79,66 @@ int main(int argc, char *argv[])
                                 printf("Size  %ld \n",sb.st_size);
                                 break;
                             case 'h':
-                                printf("Number of hard links -h  %ld\n",sb.st_nlink);
+                                printf("Number of hard links   %ld\n",sb.st_nlink);
                                 break;
                             case 'm':
-                                printf("Time of last modification -m  %s \n",ctime(&sb.st_atime));
+                                printf("Time of last modification %s",ctime(&sb.st_atime));
                                 break;
                             case 'a':
-                                printf("Access rights -a  %d %d\n",sb.st_uid,sb.st_gid);
+                                printf("Access rights %d %d\n",sb.st_uid,sb.st_gid);
                                 break;
                             case 'l':
                                 printf("Create a symbolic link give:link name -l \n");
+                                printf("Introduce the name of the new link\n");
+                                char link[100];
+                                scanf("%s",link);
+                                symlink(argv[1],link);
                                 break;
                             default:
-                                printf("Invalid option \n");
+                               printf("Invalid Operation %c\n",choice[i]);
+                                
+                               
+                               break;
+                               
+                        }
+                    }
+                    if(i!=0 && S_ISLNK(sb.st_mode))
+                    {
+                        switch(tolower(choice[i]))
+                        {
+                            case 'n':
+                                printf("Link name : %s \n",argv[1]);
                                 break;
+                            case 'l':
+                                unlink(argv[1]);   
+                                printf("Deleting the link : %s\n",argv[1]);    
+                                break;
+                            case 'd':
+                                printf("Size of the link %ld \n",sb.st_size);
+                                break;
+                            case 't':
+                                struct stat sbNew;
+                                stat(argv[1], &sbNew);
+                                printf("Size of the target %ld\n",sbNew.st_size);
+                                break;
+                            case 'a':
+                                printf("Access rights   %d %d\n",sb.st_uid,sb.st_gid);
+                                break;
+                            
+                            default:
+                              printf("Invalid Operation %c\n",choice[i]); 
+                              break;
                         }
                     }
                 }
-                else 
+                else
                 {
-                    printf("Not valid option");
+                    printf("Invalid Option %s\nTry -%s \n",choice,choice);
+
+                    break;
                 }
             i++;
+           
            }
 
 
