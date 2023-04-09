b94bad39d8c0c105c4a3de5b850eac7af44027b3
RaulBus <raul.bus@student.upt.ro>
2023-04-09 12:38:32 +0300

    HomeCommit Added: -> Function to parse files in directory  =>Regex to check if the files have .c extenstion -> Function to print access rights -> Function to print the wrong options from the Standard Input -> Menu Function for better user experience   => Organized code

--- Program.c
+++ Program.c
@@ -11,20 +11,57 @@
 #include <sys/sysmacros.h>
 #include <fcntl.h>
 #include <dirent.h>
+#include <regex.h>
+
+
 
 char choice[32];
-void ParseFileInDir(DIR *dir, int counter)
+char path[256];
+
+int ParseFileInDir(DIR *dir)
 {
-  int k;
-  struct dirent *d;
- char extension[2]=".c";
-  
-  while ((d = readdir(dir))!=NULL)
+  int counter = 0;
+  struct dirent *entry;
+  struct stat file;
+  regex_t extension;
+    if(regcomp(&extension, ".c$", REG_EXTENDED))
+    {
+        printf("Error compiling the regular expression\n");
+        exit(EXIT_FAILURE);
+    }
+ 
+  while ((entry = readdir(dir))!=NULL)
   {
-    if(strcmp(d->d_name,extension))
-        counter++;
-    closedir(dir);
+    
+    
+     
+    char *name=entry->d_name;
+    char  auxpath[4096];
+    if(strlen(name) > 2)
+    {
+        
+        strcpy(auxpath,path);
+        strcat(auxpath,name);
+        //printf("Name %s \n",auxpath);
+        if(lstat(auxpath,&file) == -1)
+        {
+            printf("Error could not get the file stats");
+            exit(EXIT_FAILURE);
+        }
+        if (S_ISREG(file.st_mode) && regexec(&extension, entry->d_name, 0, NULL, 0) == 0)
+        {
+            counter++;
+        
+        }
+    }
+   
+
+    
+        
+    
   }
+   closedir(dir);   
+   return counter;  
   
 }
 void regFileMenu(struct stat sb, char *path)
@@ -32,12 +69,12 @@ void regFileMenu(struct stat sb, char *path)
         
     printf("Regular File: %s \n", path);
     printf("Menu for regular file \n");
-    printf("1. Read -n \n");
-    printf("2. Size -d \n");
-    printf("3. Number of hard links -h \n");
-    printf("4. Time of last modification -m \n");
-    printf("5. Access rights -a \n");
-    printf("6. Create a symbolic link give:link name -l \n");
+    printf("1. Read: -n \n");
+    printf("2. Size: -d \n");
+    printf("3. Number of hard links: -h \n");
+    printf("4. Time of last modification: -m \n");
+    printf("5. Access rights: -a \n");
+    printf("6. Create a symbolic link give:link name: -l \n");
                   
            
 }
@@ -45,170 +82,303 @@ void symFileMenu(struct stat sb, char *path)
 {
         printf("Symbolic Link: %s \n", path);
         printf("Menu for symbolic link \n");
-        printf("1. Link name -n \n");
-        printf("2. Delete link -l \n");
-        printf("3. Size of the link -d \n");
-        printf("4. Size of the target -t \n");
-        printf("5. Access rights -a \n");
+        printf("1. Link name: -n \n");
+        printf("2. Delete link: -l \n");
+        printf("3. Size of the link: -d \n");
+        printf("4. Size of the target: -t \n");
+        printf("5. Access rights: -a \n");
           
 }
-void DirFileMenu(DIR *dir,char *path)
+void dirFileMenu(DIR *dir,char *path)
 {
     printf("Directory: %s \n",path);
-    printf("Menu for directory link \n");
+    printf("Menu for directory file \n");
     printf("1. Directory name : -n \n");
-    printf("2. Size of the directory -d \n");
-    printf("3. Access rights -a \n");
-    printf("4. Total number of files with .c extension \n");
+    printf("2. Size of the directory: -d \n");
+    printf("3. Access rights: -a \n");
+    printf("4. Total number of files with .c extension: -c \n");
+}
+void accessRights(struct stat sb)
+{
+    printf("Access rights for owner: \n \n");
+    if(sb.st_mode & S_IRUSR)
+        printf("Read permission for owner\n");
+    if(sb.st_mode & S_IWUSR)
+        printf("Write permission for owner\n");
+    if(sb.st_mode & S_IXUSR)
+        printf("Execute permission for owner\n");
+    printf("\nAccess rights for group: \n \n");
+    if(sb.st_mode & S_IRGRP)
+        printf("Read permission for group\n");
+    if(sb.st_mode & S_IWGRP)
+        printf("Write permission for group\n");
+    if(sb.st_mode & S_IXGRP)
+        printf("Execute permission for group\n");
+    printf("\nAccess rights for others: \n \n");
+    if(sb.st_mode & S_IROTH)
+        printf("Read permission for others\n");
+    if(sb.st_mode & S_IWOTH)
+        printf("Write permission for others\n");
+    if(sb.st_mode & S_IXOTH)
+        printf("Execute permission for others\n");
+    
+    printf("\n");
+
+    
+}
+void regularFile(int i, struct stat sb, char *fileName)
+{
+    if ( i!=0  && S_ISREG(sb.st_mode))
+    {
+        switch(tolower(choice[i]))
+        {
+            case 'n':
+                printf("File name : %s \n",fileName);
+                break;
+            case 'd':
+                printf("Size  %ld \n",sb.st_size);
+                break;
+            case 'h':
+                printf("Number of hard links   %ld\n",sb.st_nlink);
+                break;
+            case 'm':
+                printf("Time of last modification %s",ctime(&sb.st_atime));
+                break;
+            case 'a':
+                //printf("Access rights %d %d\n",sb.st_uid,sb.st_gid);
+                accessRights(sb);
+                break;
+            case 'l':
+                printf("Create a symbolic link give:link name -l \n");
+                printf("Introduce the name of the new link\n");
+                char link[100];
+                scanf("%s",link);
+                if(symlink(fileName,link) == -1)
+                {
+                    printf("Error creating the link");
+                    exit(EXIT_FAILURE);
+                }
+                printf("Link created successfully\n");
+
+                break;
+            default:
+                printf("Invalid Operation %c\n",choice[i]);
+               
+                break;
+                
+        }
+    }
+}
+void linkFile(int i, struct stat sb, char *fileName)
+{
+    if(i!=0 && S_ISLNK(sb.st_mode))
+    {
+        switch(tolower(choice[i]))
+        {
+            case 'n':
+                printf("Link name : %s \n",fileName);
+                break;
+            case 'l':
+                unlink(fileName);   
+                printf("Deleting the link : %s\n",fileName);    
+                break;
+            case 'd':
+                printf("Size of the link %ld \n",sb.st_size);
+                break;
+            case 't':
+                struct stat sbNew;
+                stat(fileName, &sbNew);
+                printf("Size of the target %ld\n",sbNew.st_size);
+                break;
+            case 'a':
+                //printf("Access rights   %d %d\n",sb.st_uid,sb.st_gid);
+                accessRights(sb);
+                break;
+            
+            default:
+                printf("Invalid Operation %c\n",choice[i]); 
+                
+                break;
+        }
+    }
+}
+void directoryFile(int i,struct stat sb,char *fileName, DIR *dir)
+{
+   
+    int nrCFiles=0;
+    //char *validCommands="ndac";
+
+    
+     if(i!= 0 && S_ISDIR(sb.st_mode))
+        {
+            switch (tolower(choice[i]))
+            {
+            case 'n':
+                printf("Directory name : %s \n",fileName);
+                break;
+            case 'd':
+                struct stat sbNew2;
+                stat(fileName,&sbNew2);
+                printf("Size of the directory: %ld \n",sbNew2.st_size);
+                break;
+            case 'a':
+                //printf("Access rights %d \n",sb.st_uid);
+               accessRights(sb);
+                break;
+            case 'c':
+            strcat(path,"/");
+            nrCFiles = ParseFileInDir(dir);
+            printf("Total number of files with .c extension : %d \n",nrCFiles);
+                
+                break;
+            default:
+                printf("Invalid option %c\n",choice[i]);
+                
+                break;
+            }
+        }
+}
+void wrongOption(char *validCommands,char *choice)
+{
+     int charIndex = 0;
+     //Check if the first character is a dash if not print the error message
+     if(choice[0] !='-')
+        {
+            printf("Try -%s \n \n",choice);
+            return;
+        }
+        //Check if the option is valid if not print the specific option that is invalid
+    while(choice[charIndex]!='\0')
+        {
+            if(strchr(validCommands,choice[charIndex]) == NULL)
+            {
+                printf("Invalid option: %c \n",choice[charIndex]);
+            }
+            charIndex++;
+        }
+        printf("\n");
+}
+void menuFunction(struct stat sb,char *path)
+{
+    DIR *dir;
+    regex_t extension;
+    char *validCommands;
+        if (S_ISREG(sb.st_mode))
+        {
+            //Call the regular file menu
+            regFileMenu(sb,path);
+
+            //Assign the valid commands for the regular file
+            validCommands = "-ndhmal";
+
+            //Compile the regular expression for the regular file
+            if(regcomp(&extension,"^-[ndhmal]+$",REG_EXTENDED) != 0)
+            {
+                
+                printf("Error compiling the regular expression");
+            }
+            
+        }
+
+        if(S_ISLNK(sb.st_mode))
+        {
+            //Call the link file menu
+            symFileMenu(sb,path);
+
+            //Assign the valid commands for the link
+            validCommands = "-nldta";
+
+            //Compile the regular expression for the link
+            if(regcomp(&extension,"^-[nldta]+$",REG_EXTENDED) != 0)
+            {
+                
+                printf("Error compiling the regular expression");
+            }
+        }
+
+        if(S_ISDIR(sb.st_mode))
+        {
+            //Open the directory 
+                dir = opendir(path);
+            //Check if the directory was opened correctly
+            if(dir == NULL)
+                {
+                    printf("Error could not open the directory");
+                }
+                //Call the directory file menu
+                dirFileMenu(dir,path);
+
+                //Assign the valid commands for the directory
+                validCommands = "-ndac";
+                //Compile the regular expression for the directory
+            if(regcomp(&extension,"^-[ndac]+$",REG_EXTENDED) != 0)
+                {
+                    
+                    printf("Error compiling the regular expression");
+                }
+                  
+               
+              
+                
+        }
+        // Get the choice from the user 
+        printf("Enter your choice: ");
+        scanf("%s",choice);
+        // Clear the screen for a better user experience 
+        system("clear");
+        
+
+        printf("Your choice %s  \n",choice);
+        // Check if the choice is valid
+        if(regexec(&extension,choice,0,NULL,0) != 0)
+        {
+            // Call the function that will print the invalid options
+           wrongOption(validCommands,choice);
+            // Recursive call to the menu function
+            menuFunction(sb,path);
+            // Reset the choice to empty string because the recurisve calls will print the choice again
+            strcpy(choice,"");
+        }
+        int i = 0;
+        while(choice[i] != '\0') 
+        {
+            //Apply the functions that were choosen
+            
+            // Regular File Functions
+            regularFile(i,sb,path);
+
+            // Link File Function
+            linkFile(i,sb,path);
+
+            // Directory File Function
+
+            directoryFile(i,sb,path,dir);
+    
+            i++;
+    
+        }
 }
 int main(int argc, char *argv[])
 {
     struct stat sb;
-    DIR *dir;
+    // Check if the user has entered the path
+    if (argc != 2) 
+    {
+        fprintf(stderr, "Usage: %s <pathname>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+        // Check if the path is a valid file
+    if (lstat(argv[1], &sb) == -1) 
+    {
+        perror("lstat");
+        
+        exit(EXIT_FAILURE);
+    }
+    // Copy the path to the path variable
+    strcpy(path,argv[1]);
+    // Call the menu function
+    menuFunction(sb,path);
     
-    char path[256];
-    int nrCFiles=0;
-
-
-           if (argc != 2) {
-               fprintf(stderr, "Usage: %s <pathname>\n", argv[0]);
-               exit(EXIT_FAILURE);
-           }
-
-           if (lstat(argv[1], &sb) == -1) {
-               perror("lstat");
-               
-               exit(EXIT_FAILURE);
-           }
-            strcpy(path,argv[1]);
-           if (S_ISREG(sb.st_mode))
-           {
-            
-            regFileMenu(sb,path);
-           }
-           if(S_ISLNK(sb.st_mode))
-            {
-            symFileMenu(sb,path);
-            }
-           if(S_ISDIR(sb.st_mode))
-            {
-                dir = opendir(path);
-                if(dir == NULL)
-                    {
-                        printf("Error could not open the directory");
-                    }
-                 DirFileMenu(dir,path);
-            }
-          
-
-            scanf("%s",choice); 
-            system("clear");
-           
-            printf("Your choice %s  \n",choice);
-           
-           int i = 0;
-           while(choice[i] != '\0') 
-           {
-            
-                if(choice[0] == '-')
-                {
-                    if ( i!=0  && S_ISREG(sb.st_mode))
-                    {
-                        switch(tolower(choice[i]))
-                        {
-                            case 'n':
-                                printf("File name : %s \n",argv[1]);
-                                break;
-                            case 'd':
-                                printf("Size  %ld \n",sb.st_size);
-                                break;
-                            case 'h':
-                                printf("Number of hard links   %ld\n",sb.st_nlink);
-                                break;
-                            case 'm':
-                                printf("Time of last modification %s",ctime(&sb.st_atime));
-                                break;
-                            case 'a':
-                                printf("Access rights %d %d\n",sb.st_uid,sb.st_gid);
-                                break;
-                            case 'l':
-                                printf("Create a symbolic link give:link name -l \n");
-                                printf("Introduce the name of the new link\n");
-                                char link[100];
-                                scanf("%s",link);
-                                symlink(argv[1],link);
-                                break;
-                            default:
-                               printf("Invalid Operation %c\n",choice[i]);
-                                
-                               
-                               break;
-                               
-                        }
-                    }
-                    if(i!=0 && S_ISLNK(sb.st_mode))
-                    {
-                        switch(tolower(choice[i]))
-                        {
-                            case 'n':
-                                printf("Link name : %s \n",argv[1]);
-                                break;
-                            case 'l':
-                                unlink(argv[1]);   
-                                printf("Deleting the link : %s\n",argv[1]);    
-                                break;
-                            case 'd':
-                                printf("Size of the link %ld \n",sb.st_size);
-                                break;
-                            case 't':
-                                struct stat sbNew;
-                                stat(argv[1], &sbNew);
-                                printf("Size of the target %ld\n",sbNew.st_size);
-                                break;
-                            case 'a':
-                                printf("Access rights   %d %d\n",sb.st_uid,sb.st_gid);
-                                break;
-                            
-                            default:
-                              printf("Invalid Operation %c\n",choice[i]); 
-                              break;
-                        }
-                    }
-                    if(i!= 0 && S_ISDIR(sb.st_mode))
-                    {
-                        switch (tolower(choice[i]))
-                        {
-                        case 'n':
-                            printf("Directory name : %s \n",path);
-                            break;
-                        case 'd':
-                            struct stat sbNew2;
-                            stat(path,&sbNew2);
-                            printf("Size of the directory: %ld \n",sbNew2.st_size);
-                            break;
-                        case 'a':
-                            printf("Access rights %d \n",sb.st_uid);
-                            break;
-                        case 'c':
-                        printf("Total number of files with .c extension\n");
-                            ParseFileInDir(dir,nrCFiles);
-                            
-                            
-                            break;
-                        default:printf("Invalid option \n");
-                            break;
-                        }
-                    }
-                }
-                else
-                {
-                    printf("Invalid Option %s\nTry -%s \n",choice,choice);
-
-                    break;
-                }
-            i++;
-           
-           }
 
 
 }
